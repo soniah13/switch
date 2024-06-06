f564aee5ad98100c815d181cb0532d2c4d3fd521
imorgansonia <imorgansonia@gmail.com>
2024-06-06 14:30:39 +0300

    first commit

---
+++ switch.js
@@ -0,0 +1,19 @@
+
+let grade="excellent";
+switch(grade){
+    case 'excellent':
+        console.log("A");
+        break;
+        case 'well done':
+            console.log("B");
+            break;
+            case 'pass':
+                console.log("C");
+                break;
+                case 'fail':
+                    console.log("D");
+                    break;
+                    default:
+                        console.log(repeat);
+
+}
